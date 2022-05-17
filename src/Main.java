a0b6a58525422fa771d829942ff75a345651cb19
habibfedaee <habib.fedaee@gmail.com>
2022-05-17 16:16:56 -0700

    solar system basic project ready

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // your code here:
+        SolarSystem factorX = new SolarSystem();
+        factorX.Sun = "1";
+        factorX.Moon = "4";
+        factorX.Planet = "2";
+        factorX.Star = "10000";
+        
+    }
+}
