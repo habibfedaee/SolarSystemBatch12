543d1877b2da4d6b6caa72c9ce4e471fa9690f9c
habibfedaee <habib.fedaee@gmail.com>
2022-05-17 17:48:36 -0700

    hey someone issue is resolved, pls merge it after your review

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
         // adding the features for stars in the solar system
         FeatureStars FacX = new FeatureStars();
-        FacX.color=green;
+        FacX.color="green";
         FacX.radius="2345j342l5j33l4km";
         FacX.size="99999999999999m";
 
